aa9b7964e13283b78b21c3e3f34355cc2c27a776
Thomas Haugli <thomas.djonne.haugli@gmail.com>
2023-09-15 10:06:26 +0200

    Handle button click

--- ui/src/main/java/ui/AppController.java
+++ ui/src/main/java/ui/AppController.java
@@ -1,12 +1,22 @@
 package ui;
 
+import core.Movie;
 import core.MovieList;
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class AppController {
+
+    @FXML private TextField titleField = new TextField();
+    @FXML private Slider scoreField = new Slider();
+    @FXML private Button addBtn = new Button();
+    @FXML private TextArea movieListField = new TextArea();
 
     private MovieList movieList;
 
@@ -35,5 +45,31 @@ public class AppController {
     }
 
     private void updateOperandsView() {
+    }
+
+    @FXML
+    private void handleAddBtn(ActionEvent event){
+        String title = this.titleField.getText();
+        double score = this.scoreField.getValue();
+
+        Movie new_movie = new Movie(title, score); 
+        movieList.addMovie(new_movie);
+
+        clearTextFields();
+        updateMovieListField();
+    }
+
+    @FXML
+    private void clearTextFields(){
+        this.titleField.setText("");
+        this.scoreField.setValue(0);
+    }
+
+    @FXML
+    private void updateMovieListField(){
+        movieListField.setText("");
+        for (Movie m : movieList.getMovies()) {
+            movieListField.setText("Title: " + m.getName() + ", score: " + m.getScore() + "/n");
+        }
     }
 }
