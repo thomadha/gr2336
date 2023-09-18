6e545011c86a98d0e7dc49b19649053fa1c86b9f
Nora Ytterbø <noraytterbo@gmail.com>
2023-09-18 10:30:59 +0200

    tester

---
+++ core/src/test/java/core/MovieListTest.java
@@ -0,0 +1,33 @@
+package core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MovieListTest {
+
+    
+    @Test 
+    @DisplayName("Checks if the list is correct")
+    public void testMovieList() throws IllegalArgumentException {
+        Movie shark = new Movie("Shark 2", 2);
+        Movie meangirls = new Movie("Mean Girls", 10);
+        Movie harry = new Movie("Harry Potter 1", 9);
+        
+        MovieList movielist = new MovieList();
+        
+        movielist.addMovie(shark);
+        movielist.addMovie(meangirls);
+        //movielist.addMovie(new Movie("WRONG", 13));
+        movielist.addMovie(harry);
+
+        Assertions.assertEquals(shark, movielist.getMovies().get(0));
+        Assertions.assertEquals(meangirls, movielist.getMovies().get(1));
+        Assertions.assertEquals(harry, movielist.getMovies().get(2));
+
+
+    }
+    
+
+
+}
--- ui/src/test/java/ui/AppTest.java
+++ ui/src/test/java/ui/AppTest.java
@@ -1,32 +1,48 @@
-package ui;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+import ui.AppController;
 
-/**
- * TestFX App test
- */
 public class AppTest extends ApplicationTest {
 
     private AppController controller;
-    private Parent root;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("App.fxml"));
-        root = fxmlLoader.load();
-        controller = fxmlLoader.getController();
-        stage.setScene(new Scene(root));
+    @BeforeEach
+    public void setUp() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/App.fxml"));
+        Scene scene = new Scene(loader.load());
+        controller = loader.getController();
+        Stage stage = new Stage();
+        stage.setScene(scene);
         stage.show();
     }
 
-    public Parent getRootNode() {
-        return root;
+    @Test
+    public void testAddMovie() {
+        TextField titleField = lookup("#titleField").query();
+        Slider scoreField = lookup("#scoreField").query();
+        Button addBtn = lookup("#addBtn").query();
+        TextArea movieListField = lookup("#movieListField").query();
+
+        // Test adding a movie
+        clickOn(titleField).write("Movie 1");
+        clickOn(scoreField).type(KeyCode.RIGHT).type(KeyCode.RIGHT); // Increase score by 2
+        clickOn(addBtn);
+        
+        // Verify if the movie has been added and the fields are cleared
+        assertEquals("", titleField.getText());
+        assertEquals(0.0, scoreField.getValue());
+        assertTrue(movieListField.getText().contains("Title: Movie 1, score: 2.0"));
     }
 }
