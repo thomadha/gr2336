35dceb75c92719a8188557ada28f32e836d2dd3c
Nora Ytterbø <noraytterbo@gmail.com>
2023-09-18 15:46:06 +0200

    update in appTest

--- ui/src/test/java/ui/AppTest.java
+++ ui/src/test/java/ui/AppTest.java
@@ -1,12 +1,16 @@
 package ui;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
@@ -19,32 +23,41 @@ import ui.AppController;
 public class AppTest extends ApplicationTest {
 
     private AppController controller;
+    private Parent root;
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/App.fxml"));
-        Scene scene = new Scene(loader.load());
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("App.fxml"));
+        root = loader.load();
         controller = loader.getController();
-        Stage stage = new Stage();
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
-    @Test
-    public void testAddMovie() {
-        TextField titleField = lookup("#titleField").query();
-        Slider scoreField = lookup("#scoreField").query();
-        Button addBtn = lookup("#addBtn").query();
-        TextArea movieListField = lookup("#movieListField").query();
-
-        // Test adding a movie
-        clickOn(titleField).write("Movie 1");
-        clickOn(scoreField).type(KeyCode.RIGHT).type(KeyCode.RIGHT); // Increase score by 2
-        clickOn(addBtn);
-        
-        // Verify if the movie has been added and the fields are cleared
-        assertEquals("", titleField.getText());
-        assertEquals(0.0, scoreField.getValue());
-        assertTrue(movieListField.getText().contains("Title: Movie 1, score: 2.0"));
+    public Parent getRootNode() {
+        return root;
     }
+
+
+    @Test
+    public void testAppInitialization() {
+        // Test the initialization of the App class
+        assertNotNull(controller);
+    }
+
+    @Test
+    public void testAddingMovie() {
+        clickOn("#titleField").write("Movie Title");
+        clickOn("#scoreField").write("5.0");
+        clickOn("#addBtn");
+        //Assertions.assertEquals();
+
+        // Add your assertions to validate the expected behavior
+        // For example, you can check if the movie list is updated correctly
+        // using controller.getMovieList() and assert the movie's presence.
+    }
+
+    
+
 }
