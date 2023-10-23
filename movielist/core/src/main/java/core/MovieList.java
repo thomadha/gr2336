aa6273ca00899572b3ad82bfb164953246bdb7a4
mathildelykke <mathilde.lykke@gmail.com>
2023-10-23 08:44:44 +0200

    Implement password for better security

--- movielist/core/src/main/java/core/MovieList.java
+++ movielist/core/src/main/java/core/MovieList.java
@@ -8,13 +8,22 @@ public class MovieList {
   private List<Movie> movies;
 
   private String username; 
+  private String password; 
 
   public String getUsername() {
     return username;
   }
 
+  public String getPassword() {
+    return password;
+  }
+
   public void setUsername(String username) {
     this.username = username;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
   }
 
   public void setMovies(List<Movie> movies){
