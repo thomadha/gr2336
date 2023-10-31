cdf5fb44295763361b9bc78307e7f09029802b1c
Nora Ytterbø <noraytterbo@gmail.com>
2023-10-31 10:47:27 +0100

    #59: fixed poms and module-info i springboot to set up localhost:8080/movielist, co-author: Thomas

--- movielist/springboot/src/main/java/module-info.java
+++ movielist/springboot/src/main/java/module-info.java
@@ -1,13 +1,14 @@
 module movielist.springboot {
 
-  requires com.google.gson
+  requires com.google.gson;
   requires spring.web;
   requires spring.boot;
   requires spring.context;
   requires spring.boot.autoconfigure;
+  requires spring.beans;
 
-  requires movielist.core;
-  requires movielist.json;
+  // requires movielist.core;
+  // requires movielist.json;
 
   opens movielist.springboot to movielist.springboot, spring.beans, spring.web, spring.context, com.google.gson;
   
--- movielist/springboot/src/main/java/movielist/springboot/MovieListRestController.java
+++ movielist/springboot/src/main/java/movielist/springboot/MovieListRestController.java
@@ -1,4 +1,4 @@
-
+package movielist.springboot;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
