8793dc3b95a30321d7fedd05b808a2abc5600f42
Nora Ytterbø <noraytterbo@gmail.com>
2023-11-09 10:28:27 +0100

    #60: Further bugfixing for checkstyle in Movie.java, MovieList.java and created a package-info.java in same folder

--- movielist/core/src/main/java/core/Movie.java
+++ movielist/core/src/main/java/core/Movie.java
@@ -1,14 +1,34 @@
 package core;
 
 public class Movie {
-  private String name; 
-  private String genre; 
+
+  /**
+   * Name of movie.
+   */
+  private String name;
+  /**
+   * Genre of movie.
+   */
+  private String genre;
+  /**
+   * Score of movie.
+   */
   private double score;
+  /**
+   * Amount of times a movie has been watched.
+   * Starts at 1.
+   */
   private int movieCount = 1;
 
   /**
+   * Maximum value of score
+   */
+  public static final int MAXIMUM_SCORE = 10;
+
+
+  /**
    * toString method for showing objects of the Movie Class.
-   * 
+   *
    * @return a string with name, genre, score and moviecount.
    */
   @Override
@@ -18,72 +38,72 @@ public class Movie {
 
   /**
    * Creates a string for showing scores.
-   * 
+   *
    * @return a string with name of movie and score.
    */
-  public String scoreString(){
+  public String scoreString() {
     return name + "\n - Score: " + score + "\n\n";
   }
 
   /**
    * Creates a string for showing scores.
-   * 
+   *
    * @return a string with name of movie and score.
    */
-  public String viewsString(){
+  public String viewsString() {
     return name + "\n - Views: " + movieCount + "\n\n";
   }
 
   /**
    * Constructor for movie class without movieCount.
-   * 
+   *
    * @param name of movie.
    * @param score of movie.
    * @param genre of movie.
    */
-  public Movie(String name, double score, String genre){
-    if(!validScore(score)){
+  public Movie(final String name, final double score, final String genre) {
+    if (!validScore(score)) {
       throw new IllegalArgumentException("Score must be between 0 and 10");
     }
-    if(!validName(name)){
+    if (!validName(name)) {
       throw new IllegalArgumentException("Must have a name");
     }
-    if(!validgenre(genre)){
+    if (!validgenre(genre)) {
       throw new IllegalArgumentException("Not specified");
     }
     this.name = name;
-    this.score = score; 
-    this.genre=genre;
+    this.score = score;
+    this.genre = genre;
   }
 
   /**
    * Constructor for movie class with movieCount.
-   * 
+   *
    * @param name of movie.
    * @param score of movie.
    * @param genre of movie.
    * @param movieCount which is amount of times a movie has been seen.
    */
-  public Movie(String name, double score, String genre, int movieCount){
-    if(!validScore(score)){
+  public Movie(final String name, final double score, final String genre, final int movieCount) {
+    if (!validScore(score)) {
       throw new IllegalArgumentException("Score must be between 0 and 10");
     }
-    if(!validName(name)){
+    if (!validName(name)) {
       throw new IllegalArgumentException("Must have a name");
     }
-    if(!validgenre(genre)){
+    if (!validgenre(genre)) {
       this.genre = "Not specified";
-    }else{
+    } else {
       this.genre = genre;
     }
     this.name = name;
     this.score = score;
     this.movieCount = movieCount;
   }
-  
+
   /**
    * Getter for moviename.
-   * 
+   *
    * @return String of moviename.
    */
   public String getName() {
@@ -91,7 +111,7 @@ public class Movie {
   }
   /**
    * Getter for genre.
-   * 
+   *
    * @return String of genre.
    */
   public String getGenre() {
@@ -99,7 +119,7 @@ public class Movie {
   }
   /**
    * Getter for score.
-   * 
+   *
    * @return double of moviename.
    */
   public double getScore() {
@@ -107,70 +127,72 @@ public class Movie {
   }
   /**
    * Setter for name.
-   * 
-   * @param string of moviename.
+   *
+   * @param name of movie.
    */
-  public void setName(String name) {
+  public void setName(final String name) {
     this.name = name;
   }
   /**
    * Setter for score.
-   * 
-   * @param double of score.
+   *
+   * @param score
    */
-  public void setScore(double score) {
+  public void setScore(final double score) {
     this.score = score;
   }
   /**
    * Setter for genre.
-   * 
-   * @param string of genre.
+   *
+   * @param genre
    */
-  public void setGenre(String genre) {
+  public void setGenre(final String genre) {
     this.genre = genre;
   }
 
   /**
-   * Checks if score is valid, i.e over or equal to 0.0 and below or equal to 10.0.
-   * 
+   * Checks if score is valid.
+   * Valid: over or equal to 0.0 and below or equal to 10.0.
+   *
    * @param score of movie.
    * @return a boolean, true if valid and false if not.
    */
-  private boolean validScore(double score){
-    return score<=10 && score>=0;
+  private boolean validScore(final double score) {
+    return score <= MAXIMUM_SCORE && score >= 0;
   }
+
   /**
    * Checks if name is valid. Meaning not an empty string.
-   * 
+   *
    * @param name of movie.
    * @return a boolean, true if valid and false if not.
    */
-  private boolean validName(String name){
+  private boolean validName(final String name) {
     return !name.isEmpty();
   }
 
   /**
    * Checks if genre is valid.
-   * 
+   *
    * @param genre of movie.
    * @return a boolean, true if valid and false if not.
    */
-  private boolean validgenre(String genre){
+  private boolean validgenre(final String genre) {
     return !genre.equals("Choose genre");
   }
 
   /**
    * Method to increase moviecount if two movies have the same name.
    */
-  public void addDuplicate(){
-    this.movieCount ++;
+  public void addDuplicate() {
+    this.movieCount++;
   }
 
   /**
    * Getter for moviecount.
    * @return movieCount of object.
    */
-  public int getCount(){
+  public int getCount() {
     return this.movieCount;
   }
 }
--- movielist/core/src/main/java/core/MovieList.java
+++ movielist/core/src/main/java/core/MovieList.java
@@ -5,14 +5,23 @@ import java.util.Comparator;
 import java.util.List;
 
 public class MovieList {
-  private List<Movie> movies;
 
+  /**
+   * Paramater for list of movies.
+   */
+  private List<Movie> movies;
+  /**
+   * Paramater for a string username.
+   */
   private String username;
+  /**
+   * Paramater for a string password.
+   */
   private String password;
 
   /**
    * Getter for username of movielist.
-   * 
+   *
    * @return a string of username.
    */
   public String getUsername() {
@@ -21,7 +30,7 @@ public class MovieList {
 
   /**
    * Getter for password of the movielist.
-   * 
+   *
    * @return a string of password.
    */
   public String getPassword() {
@@ -30,54 +39,54 @@ public class MovieList {
 
   /**
    * Setter for username.
-   * 
+   *
    * @param username of object.
    */
-  public void setUsername(String username) {
+  public void setUsername(final String username) {
     this.username = username;
   }
 
   /**
    * Setter for password.
-   * 
+   *
    * @param password of object.
    */
-  public void setPassword(String password) {
+  public void setPassword(final String password) {
     this.password = password;
   }
 
   /**
    * Setter for movies.
-   * 
+   *
    * @param movies in a list.
    */
-  public void setMovies(List<Movie> movies){
+  public void setMovies(final List<Movie> movies) {
     this.movies = new ArrayList<>(movies);
   }
 
   /**
    * Constructor for this class.
    */
-  public MovieList(){
+  public MovieList() {
     this.movies = new ArrayList<Movie>();
   }
 
   /**
    * Getter for movies.
-   * 
+   *
    * @return ArrayList of movies for object.
    */
   public List<Movie> getMovies() {
     return new ArrayList<Movie>(movies);
   }
-  
+
   /**
    * Method to add a movie to the movielist object.
-   * 
+   *
    * @param movie to add.
    */
-  public void addMovie(Movie movie){
-    if(!checkDuplicate(movie)){
+  public void addMovie(final Movie movie) {
+    if (!checkDuplicate(movie)) {
       movies.add(movie);
     }
     movies.sort(Comparator.comparingDouble(Movie::getScore).reversed());
@@ -86,39 +95,39 @@ public class MovieList {
   /**
    * Method to sort list by amount of times the movie's been watched.
    */
-  public void sortByCount(){
+  public void sortByCount() {
     movies.sort(Comparator.comparingInt(Movie::getCount).reversed());
   }
 
   /**
    * Method to sort movies in list by best rating.
    */
-  public void sortByBestRating(){
+  public void sortByBestRating() {
     movies.sort(Comparator.comparingDouble(Movie::getScore).reversed());
   }
 
   /**
    * Method to sort movies in list by worst rating.
    */
-  public void sortByWorstRating(){
+  public void sortByWorstRating() {
     movies.sort(Comparator.comparingDouble(Movie::getScore));
   }
 
   /**
    * Getter for number of movies.
-   * 
+   *
    * @return number of movies in list.
    */
-  public int getNumberOfMovies(){
+  public int getNumberOfMovies() {
     return movies.size();
   }
 
   /**
    * A toString similar method for the class Movielist.java.
-   * 
+   *
    * @return a string of movies in MovieList object.
    */
-  public String movieListToString(){
+  public String movieListToString() {
     String s = "";
     for (Movie movie : movies) {
       s += movie.getName() + ", " + movie.getScore() + "\n";
@@ -127,16 +136,17 @@ public class MovieList {
   }
 
   /**
-   * Method to check if a movie is already in the list, if so it updates the movie with mean score and new genre.
+   * Method to check if a movie is already in the list.
+   * If so it updates the movie with mean score and new genre.
    * @param newMovie to be added to the list.
    * @return true if movie was a duplicate, else false.
    */
-  public boolean checkDuplicate(Movie newMovie){
+  public boolean checkDuplicate(final Movie newMovie) {
     boolean hasDuplicate = false;
     String newTitle = newMovie.getName();
-    for(Movie m : getMovies()){
+    for (Movie m : getMovies()) {
       String oldTitle = m.getName();
-      if (oldTitle.equals(newTitle)){
+      if (oldTitle.equals(newTitle)) {
         Double newScore = ((m.getScore() + newMovie.getScore()) / 2);
         String newGenre = newMovie.getGenre();
         m.setScore(newScore);
