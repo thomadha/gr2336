5f75471bd9bf13da0729486d886d7ea8bb0b2f97
Nora Ytterbø <noraytterbo@gmail.com>
2023-11-16 02:51:42 +0100

    #74: checkstyle works for movielistlocalaccess

--- movielist/ui/src/main/java/dataaccess/MovieListLocalAccess.java
+++ movielist/ui/src/main/java/dataaccess/MovieListLocalAccess.java
@@ -1,12 +1,14 @@
 package dataaccess;
 
-import java.util.List;
-
 import core.Movie;
 import core.MovieList;
 import filehandler.MovieListHandler;
+import java.util.List;
 
-public class MovieListLocalAccess implements MovieListAccess{
+/**
+ * Class for local movielist access.
+ */
+public class MovieListLocalAccess implements MovieListAccess {
 
   private MovieList movieList;
   private MovieListHandler fileHandler;
@@ -14,7 +16,7 @@ public class MovieListLocalAccess implements MovieListAccess{
   public MovieListLocalAccess(MovieList movielist) {
     movieList = new MovieList();
     fileHandler = new MovieListHandler("/src/main/java/json/MovieList.json");
-}
+  }
 
   @Override
   public MovieList getMovieList() {
@@ -26,21 +28,9 @@ public class MovieListLocalAccess implements MovieListAccess{
     return fileHandler.getMovieList(username);
   }
 
-  public void updateMovieList(MovieList newMovieList){
+  public void updateMovieList(MovieList newMovieList) {
     this.movieList = newMovieList;
   }
-
-  // /**
-  //  * Gets the names of the MovieLists.
-  //  *
-  //  * @return the names of the MovieLists.
-  //  */
-  // @Override
-  // public Collection<String> getMovieListNames() {
-  //   Collection<String> allNames = new ArrayList<>();
-  //   fileHandler.getAllMovieListsFromFile().forEach(movieList -> allNames.add(movieList.getUsername()));
-  //   return allNames;
-  // }
 
 
   @Override
@@ -58,48 +48,11 @@ public class MovieListLocalAccess implements MovieListAccess{
     fileHandler.removeMovieList(fileHandler.getMovieList(username));
   }
 
-  /**
-   * Updates the user file with the specified user.
-   *
-   * @param user the user to update
-   */
-
-
-  // @Override
-  // public void validateNoExistingMovieList(String username) {
-  //   fileHandler.validateNoExistingMovieList(username);
-  // }
 
   @Override
   public void addMovieToList(Movie movie) {
     movieList.addMovie(movie);
     fileHandler.saveToFile(movieList);
   }
-
-  // @Override
-  // public List<Movie> getMovies() {
-  //   return movieList.getMovies();
-  // }
-
-
-  // @Override
-  // public boolean checkDuplicate(Movie newMovie) {
-  //   return movieList.checkDuplicate(newMovie);
-  // }
-
-  // @Override
-  // public void setUsername(String username) {
-  //   movieList.setUsername(username);
-  // }
-
-  // @Override
-  // public void setPassword(String password) {
-  //   movieList.setPassword(password);
-  // }
-
-  // @Override
-  // public void setMovies(List<Movie> movies) {
-  //   movieList.setMovies(movies);
-  // }
 
 }
