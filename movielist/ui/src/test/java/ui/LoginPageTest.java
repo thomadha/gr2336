60267448edd39729a3b765d86b2e8d709ac07477
Isak Tønnesen <isak.tonnesen@ntnu.no>
2023-11-16 17:20:47 +0100

    #81 checkstyle fixes

--- movielist/ui/src/test/java/ui/LoginPageTest.java
+++ movielist/ui/src/test/java/ui/LoginPageTest.java
@@ -1,44 +1,40 @@
 package ui;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.testfx.api.FxAssert.verifyThat;
-
-import javafx.scene.input.MouseButton;
-import org.testfx.util.WaitForAsyncUtils;
-import javafx.scene.control.DialogPane;
-import static org.testfx.matcher.control.LabeledMatchers.hasText;
-import javafx.scene.control.ButtonType;
-
-import javafx.scene.control.MenuButton;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import javafx.scene.Node;
-import java.lang.reflect.InvocationTargetException;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
 
+import java.lang.reflect.InvocationTargetException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
-import javafx.stage.Stage;
+import org.testfx.util.WaitForAsyncUtils;
+
 import ui.AppController;
 import ui.LoginController;
-import javafx.scene.text.Text;
-import javafx.scene.control.Label;
 
 public class LoginPageTest extends ApplicationTest {
 
