ef3276e92cbdef4b4c16a25c754836da74488ae9
Nora Ytterbø <noraytterbo@gmail.com>
2023-11-16 01:52:25 +0100

    #74: Checkstyle fixed for springbootapplication.

--- movielist/springboot/src/main/java/movielist/springboot/SpringbootApplication.java
+++ movielist/springboot/src/main/java/movielist/springboot/SpringbootApplication.java
@@ -3,11 +3,14 @@ package movielist.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Class for launching the springboot application.
+ */
 @SpringBootApplication
 public class SpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootApplication.class, args);
+  }
 
 }
