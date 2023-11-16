9660d2fc7d87600fb0fc47cd297b0c928abb36f5
Nora Ytterbø <noraytterbo@gmail.com>
2023-11-16 01:20:23 +0100

    #74 checkstyle finished for file

--- movielist/springboot/src/main/java/movielist/springboot/MovieListRestController.java
+++ movielist/springboot/src/main/java/movielist/springboot/MovieListRestController.java
@@ -1,23 +1,23 @@
 package movielist.springboot;
+
+import com.google.gson.Gson;
+import core.Movie;
+import core.MovieList;
+import java.io.IOException;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.gson.Gson;
 
-import java.io.IOException;
-import java.util.List;
-
-import core.Movie;
-import core.MovieList;
-
-import org.springframework.web.bind.annotation.PathVariable;
-
-
+/**
+ * Rest controller class for MovieList.
+ */
 @Configuration
 @RestController
 @RequestMapping("/movielist")
@@ -29,6 +29,7 @@ public class MovieListRestController {
   //   return new MovieListService(); 
   // }
 
+
   public MovieListRestController(){
     movielistservice = new MovieListService();
   }
@@ -37,16 +38,21 @@ public class MovieListRestController {
 
   //localhost:8080/movielist/getall
   @GetMapping("/getall")
-  public List<MovieList> getAllMovieLists(){
+  public List<MovieList> getAllMovieLists() {
     return movielistservice.getMovieLists(); 
   }
 
   //localhost:8080/movielist/{username}
   @GetMapping("/{username}")
   public MovieList getMovieListbyname(@PathVariable("username") String username) {
-      return movielistservice.getMovieListByUsername(username); 
+    return movielistservice.getMovieListByUsername(username); 
   }
 
+  /**
+   * A method adding movielist to a given body.
+   *
+   * @param body body
+   */
   //localhost:8080/movielist/add
   @PostMapping("/add")
   public void addMovieList(@RequestBody String body) {
@@ -61,9 +67,18 @@ public class MovieListRestController {
     movielistservice.deleteMovieList(username);
   }
 
-//localhost:8080/movielist/{username}/addMovie
+  /**
+   * A method that adds a movie to movielist.
+   *
+   * @param body Takes inn a body element.
+   * @param username Takes in a username String element.
+   * @return A movie element.
+   * @throws IOException Throws exception if wrongful input
+   */
+  //localhost:8080/movielist/{username}/addMovie
   @PostMapping("/{username}/addMovie")
-  public Movie addMovie(@RequestBody String body, @PathVariable("username") String username) throws IOException{
+  public Movie addMovie(@RequestBody String body, 
+      @PathVariable("username") String username) throws IOException {
     Gson gson = new Gson();
     Movie movie = gson.fromJson(body, Movie.class);
     movielistservice.addMovieToList(username, movie);
@@ -72,7 +87,8 @@ public class MovieListRestController {
 
   //localhost:8080/movielist/{username}/{movieTitle}
   @GetMapping("/{username}/{movieTitle}")
-  public Movie getMovie(@PathVariable("movieTitle") String movieTitle, @PathVariable("username") String username) throws IOException{
+  public Movie getMovie(@PathVariable("movieTitle") String movieTitle,
+        @PathVariable("username") String username) throws IOException {
     return movielistservice.getMovie(username, movieTitle);
   }
 
@@ -90,7 +106,8 @@ public class MovieListRestController {
 
   //localhost:8080/movielist/{username}/password/newUser
   @PostMapping("/{username}/{password}/newUser")
-  public MovieList newMovieList(@PathVariable("username") String username, @PathVariable("password") String password){
+  public MovieList newMovieList(@PathVariable("username") String username,
+        @PathVariable("password") String password){
     return movielistservice.newMovieList(username, password);
   }
 }
